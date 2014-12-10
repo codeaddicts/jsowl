ca34692b91c409af94bda53e00f5304618d64c2a
SplittyDev <splittydev@gmail.com>
2014-12-10 07:34:41 +0100

    Added the jsowl-cgi project

---
+++ src/jsowl-cgi/Compiler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using libjsowl;
+
+namespace jsowlcgi
+{
+	public class Compiler
+	{
+		Lexer lex;
+		CodeGen gen;
+		Beautifier sexify;
+
+		public Compiler () {
+			lex = new Lexer ((reason) => { Console.WriteLine ("// ERROR."); });
+			gen = new CodeGen (CompilerOptions.None);
+			sexify = new Beautifier ();
+		}
+
+		public string Run (string content) {
+			string src;
+			lex.FeedSource (content);
+			src = gen.Feed (lex.tokens);
+			src = sexify.Feed (src);
+			return src;
+		}
+	}
+}
+
---
+++ src/jsowl-cgi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace jsowlcgi
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			Console.Write ("Content-Type: text/javascript\n\n");
+			string path = Environment.GetEnvironmentVariable ("PATH_TRANSLATED");
+			if (File.Exists (path)) {
+				string input = string.Empty;
+				using (FileStream FILE = new FileStream (path, FileMode.Open)) {
+					using (StreamReader reader = new StreamReader (FILE)) {
+						input = reader.ReadToEnd ();
+					}
+				}
+				Compiler compiler = new Compiler ();
+				string src = compiler.Run (input);
+				Console.WriteLine (src);
+			}
+		}
+	}
+}
