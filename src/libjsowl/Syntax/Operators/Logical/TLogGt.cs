d58af3116c0c6917b1f1e52f06f419aa9cd98c4e
SplittyDev <splittydev@gmail.com>
2014-12-10 03:48:23 +0100

    Added more operators, added token index

---
+++ src/libjsowl/Syntax/Operators/Logical/TLogGt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace libjsowl
+{
+	public class TLogGt : Token
+	{
+		public TLogGt (int line) : base (line)
+		{
+		}
+
+		public override string ToString ()
+		{
+			return string.Format (">");
+		}
+	}
+}
+
