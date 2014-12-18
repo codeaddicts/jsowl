a7e9a7cf5ea357ba9253190b0f780a55d21d994c
SplittyDev <splittydev@gmail.com>
2014-12-18 14:32:01 +0100

    Did some cleanup work Prepared the IScanner interface for the upcoming change in the Lexer

--- src/libjsowl/Interfaces/IScanner.cs
+++ src/libjsowl/Interfaces/IScanner.cs
@@ -8,8 +8,13 @@ namespace libjsowl
 		/// Checks if this scanner matches the current character in the source stream.
 		/// </summary>
 		/// <returns><c>true</c>, if this scanner can scan the current character, <c>false</c> otherwise.</returns>
-		bool canScan ();
-		TToken Scan ();
+		bool canScan (ref SourceStream stream);
+
+		/// <summary>
+		/// Scan a token.
+		/// </summary>
+		/// <returns>The scanned token</returns>
+		TToken Scan (ref SourceStream stream);
 	}
 }
 
